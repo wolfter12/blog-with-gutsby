1211f1dfea3c464072c600286b8972b8383149e0
Nazar <wolfter12@gmail.com>
2022-01-06 18:54:05 +0200

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = ({ data }) => {
       <Seo title="Home" />
       <div>
         <h1>Nazar's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <BlogLink to={node.fields.slug}>
